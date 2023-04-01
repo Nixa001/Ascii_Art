746b0a66fb187558a7abd02c585cd45883a48d2a
Nicolas Faye <85998620+Nixa001@users.noreply.github.com>
2023-04-01 17:11:31 +0000

    Ascii-art-fs

---
+++ ascii-art-fs.go
@@ -0,0 +1,15 @@
+package functions
+
+import (
+	"os"
+)
+
+func Takebanner() string {
+	args := os.Args
+	value := "banner/standard.txt"
+	if len(args) == 3 {
+		value = "banner/" + os.Args[2] + ".txt"
+		return value
+	}
+	return value
+}
