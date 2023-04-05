890ee6218efe75d16efee481dc1e8609620058ee
Nicolas Faye <85998620+Nixa001@users.noreply.github.com>
2023-04-05 04:36:20 -0700

    Fix: Invalid banner

--- main.go
+++ main.go
@@ -1,46 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
-var tabChars = map[int][]string{}
-var taille = 9
+var tabChar = map[int][]string{}
+var HeightChar = 9
 
 func main() {
 	args := os.Args
 	arg1 := os.Args[1]
 	if len(args) < 2 || len(args) > 3 {
-		fmt.Println("Invalid command")
-		fmt.Println("Usage: go run . \"Input string\" [Banner]")
+		fmt.Println("Usage: go run . [STRING] [BANNER]")
+		fmt.Println("\nEX: go run . something standard")
 		os.Exit(0)
 	}
 	ReadAsciiTab()
 	GenerateAscii(arg1)
 }
+
 func ReadAsciiTab() {
 	arg2 := Takebanner()
-	data, _ := ioutil.ReadFile(arg2)
-	dataChar := strings.Split(string(data), "\n")
-	NumberChars := len(dataChar) / taille
-	for i := 0; i < NumberChars; i++ {
-		character := dataChar[i*taille : (i+1)*taille]
-		tabChars[int(i)] = character
+	var tabChars []string
+	scanner, err := ioutil.ReadFile(arg2)
+	if err != nil {
+		fmt.Println("Invalid banner")
+		os.Exit(1)
+	}
+	data := bufio.NewScanner(strings.NewReader(string(scanner)))
+	for data.Scan() {
+		lines := data.Text()
+		tabChars = append(tabChars, lines)
+	}
+	characters := len(tabChars) / HeightChar
+	for i := 0; i < characters; i++ {
+		charLines := tabChars[i*HeightChar : (i+1)*HeightChar]
+		tabChar[int(i)] = charLines
+
 	}
 }
+
 func GenerateAscii(input string) {
 	var output string
 	divInput := strings.Split(input, "\\n")
 	for _, InputPart := range divInput {
 
-		for i := 1; i < taille; i++ {
+		for i := 1; i < HeightChar; i++ {
 			for _, runeValue := range InputPart {
 				if int(runeValue) >= 32 && int(runeValue) <= 126 {
 					NumCharsInTab := int(runeValue) - 32
-					outputLine := tabChars[NumCharsInTab][i]
+					outputLine := tabChar[NumCharsInTab][i]
 					if outputLine != "" {
 						output += outputLine
 					}
