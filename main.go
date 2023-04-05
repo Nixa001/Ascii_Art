a9e86b1a665c4eb418227a4b120474c49b9c9a26
Nicolas Faye <85998620+Nixa001@users.noreply.github.com>
2023-04-05 04:09:14 -0700

    style and Feat: Display Thinkertoy

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
-var tabChars = map[int][]string{}
-var taille = 9
+var tabChar = map[int][]string{}
+var HeightChar = 9
 
 func main() {
 	args := os.Args
@@ -19,29 +20,35 @@ func main() {
 		os.Exit(0)
 	}
 	ReadAsciiTab()
-	output:=GenerateAscii(arg1)
+	output := GenerateAscii(arg1)
 	asciiArtOutput(output)
 }
 func ReadAsciiTab() {
 	arg2 := asciiArtFs()
-	data, _ := ioutil.ReadFile(arg2)
-	dataChar := strings.Split(string(data), "\n")
-	NumberChars := len(dataChar) / taille
-	for i := 0; i < NumberChars; i++ {
-		character := dataChar[i*taille : (i+1)*taille]
-		tabChars[int(i)] = character
+	var tabChars []string
+	scanner, _ := ioutil.ReadFile(arg2)
+	data := bufio.NewScanner(strings.NewReader(string(scanner)))
+	for data.Scan() {
+		lines := data.Text()
+		tabChars = append(tabChars, lines)
+	}
+	characters := len(tabChars) / HeightChar
+	for i := 0; i < characters; i++ {
+		charLines := tabChars[i*HeightChar : (i+1)*HeightChar]
+		tabChar[int(i)] = charLines
+
 	}
 }
-func GenerateAscii(input string) string{
+func GenerateAscii(input string) string {
 	var output string
 	divInput := strings.Split(input, "\\n")
 	for _, InputPart := range divInput {
 
-		for i := 1; i < taille; i++ {
+		for i := 1; i < HeightChar; i++ {
 			for _, runeValue := range InputPart {
 				if int(runeValue) >= 32 && int(runeValue) <= 126 {
 					NumCharsInTab := int(runeValue) - 32
-					outputLine := tabChars[NumCharsInTab][i]
+					outputLine := tabChar[NumCharsInTab][i]
 					if outputLine != "" {
 						output += outputLine
 					}
@@ -53,7 +60,7 @@ func GenerateAscii(input string) string{
 			}
 			if InputPart != "" {
 				output += "\n"
-				
+
 			}
 		}
 	}
